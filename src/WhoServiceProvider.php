30cd93f921197695bf2a7e62d9bcfb650c7d70af
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-23 15:46:27 +0800

    Automated Update 2026-07-23 15:46

--- src/WhoServiceProvider.php
+++ src/WhoServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Rimba\Who;
 
-use Rimba\Base\BitesServiceProvider;
+use Rimba\Base\Services\BitesServiceProvider;
 
 class WhoServiceProvider extends BitesServiceProvider
 {
