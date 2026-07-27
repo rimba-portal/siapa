9007a1630cf7a029a3ab286e2b938204cd4d8f31
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-27 10:13:26 +0800

    Update 2026-07-27 10:13

--- src/WhoServiceProvider.php
+++ src/WhoServiceProvider.php
@@ -6,24 +6,23 @@ namespace Rimba\Who;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class WhoServiceProvider extends BitesServiceProvider
 {
-    protected string $configFile = __DIR__.'/../config/bites.php';
-
-    protected string $viewsPath = __DIR__.'/../resources/views';
-
-    protected string $iconsPath = __DIR__.'/../resources/svg';
+    protected string $configFile = __DIR__ . '/../config/bites.php';
+    protected string $viewsPath = __DIR__ . '/../resources/views';
+    protected string $iconsPath = __DIR__ . '/../resources/svg';
 
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->publishes([__DIR__.'/../resources/assets/models' => public_path('models')], 'assets');
+        $this->publishes([__DIR__.'/../resources/assets/models' => public_path('models'),], 'assets');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
